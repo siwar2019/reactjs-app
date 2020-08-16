76f08be8f41d612ed1debd79148d3b74db14b561
siwar2019 <siwarbougrine@gmail.com>
2020-08-16 16:34:13 +0200

    liste des favoris check

--- src/components/DataTable2.js
+++ src/components/DataTable2.js
@@ -1,31 +1,39 @@
 import React, { Component } from 'react';
-import axios from "axios"
+import axios from "axios" ;
+import { IoMdHeartEmpty,IoMdHeart,IoIosRemoveCircleOutline } from "react-icons/io";
 
 
 class DataTable2 extends Component {
   handleDelete = () => {
     axios.delete("http://192.168.43.41:5000/delete/" + this.props.obj.id)
         .then(res => {
+         
             console.log(res.data);
         })
 }
-confiant = () => {
-  axios.post("http://192.168.43.41:5000/confiantuser2/" + this.props.obj.id)
-      .then(res => {
-          console.log(res.data);
-      })
+//
+
+modifyIsConfident = isConfidentValue => {
+  return axios
+    .put("http://192.168.43.41:5000/updateIsConfident/"+ this.props.obj.id, {
+      isConfident: isConfidentValue,
+    })
+    .then(response => {
+      console.log('Registered')
+    })
 }
-   /*  handleDelete = ()=>{
-     //5ater 3andi proxy 
-                 axios.delete("/delete/"+this.props.obj.id)
-                 .then(res => {
-                   console.log(res.data);
-                 
-                 })
-    }
- */
+confiant = () => {
+  /* axios.post("http://192.168.43.41:5000/confiantuser2/" + this.props.obj.id)
+      .then(res => {
+          console.log(res.data); */
+          console.log('on clik fnction') 
+//       })
+ }
+  
     
     render() {
+ 
+
       const button = {
         
       color: "black",
@@ -46,14 +54,34 @@ confiant = () => {
                  <td>  {this.props.obj.address} </td>
                  <td>  {this.props.obj.tel} </td>
                  <td >
-                    <form onSubmit={this.handleEditSubmit}>
-                        <button style={button} type="submit" value={this.props.obj.id} onClick={this.handleDelete}>Block</button>
+                    <form >
+                        <button style={{color:'wite',raduis:'20',borderRadius:30}}>
+                        <IoIosRemoveCircleOutline style={{color:'red',width:'25',height:'25'}}
+
+                         value={this.props.obj.id} onClick={this.handleDelete}> 
+                        </IoIosRemoveCircleOutline>
+                        </button>
+                        
+                     
                     </form>
                 </td>
                 <td >
-                    <form onSubmit={this.handleEditSubmit}>
-                        <button style={button} type="submit" value={this.props.obj.id} onClick={this.confiant}>Favoris</button>
-                    </form>
+                   
+                      {
+                        this.props.obj.isConfident ?<IoMdHeart style={{color:'red',width:"25",height:"25"}} onClick={
+                         async () => {
+                           await this.modifyIsConfident(false)
+                          }
+                        }>delete user from list confident></IoMdHeart>
+                         : 
+                         <IoMdHeartEmpty  value={this.props.obj.id} style={{width:"25",height:"25"}} onClick={
+                          async () => {
+                            await this.modifyIsConfident(true)
+                           }
+                          } >Favoris></IoMdHeartEmpty>
+
+                      }
+                   
                 </td>
             </tr>
         );
